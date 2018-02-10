7ce9f32dcd29849d568585ffc403e8b589489b25
Chloe Hsu <chingyunhsu22@gmail.com>
2018-02-10 15:28:20 -0800

    fixed a bug in computing rotation angle

--- compute_holomorphic_stress.m
+++ compute_holomorphic_stress.m
@@ -9,10 +9,11 @@ scaling = 1 + sum(T .* directions, 2) ./ (twodnorm(directions) .^ 2);
 new_directions = directions + T;
 cross_prod = directions(:,1).*new_directions(:,2) - ...
     new_directions(:,1).*directions(:,2);
-rotation_sin = cross_prod ./ twodnorm(directions) ./ twodnorm(directions);
+rotation_sin = cross_prod ./ twodnorm(directions) ./ twodnorm(new_directions);
 rotation_sin(isnan(rotation_sin))=0;
 rotation_cos = sqrt(1 - rotation_sin .* rotation_sin);
 
+disp(rotation_sin);
 g_on_controls = complex((2*scaling-1) .* rotation_cos, (2*scaling-1) .* rotation_sin);
 g = W * g_on_controls;
 
