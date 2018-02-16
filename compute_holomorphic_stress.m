7c038e794260c567bd3286b95130e8ba36a10daf
Chloe Hsu <chingyunhsu22@gmail.com>
2018-02-16 13:17:47 -0800

    fixed bug in stress function setup

--- compute_holomorphic_stress.m
+++ compute_holomorphic_stress.m
@@ -13,7 +13,6 @@ rotation_sin = cross_prod ./ twodnorm(directions) ./ twodnorm(new_directions);
 rotation_sin(isnan(rotation_sin))=0;
 rotation_cos = sqrt(1 - rotation_sin .* rotation_sin);
 
-disp(rotation_sin);
 g_on_controls = complex((2*scaling-1) .* rotation_cos, (2*scaling-1) .* rotation_sin);
 g = W * g_on_controls;
 
